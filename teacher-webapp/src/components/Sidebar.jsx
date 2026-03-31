0817592398735fb727246dfb36fd51e01c6e652b
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2026-03-31 11:58:35 -0500

    remove web folder and node_modules

--- teacher-webapp/src/components/Sidebar.jsx
+++ teacher-webapp/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from "react"
-import pictr from "./assets/Eduvisionlogo.png"
+
 
 function Sidebar({setPage}){
   return(
     
     <div className="sidebar">
         <h1>
-        <img className = "logo" src={pictr}></img>
+       
     </h1>
       <h2>Sidebar</h2>
       <button onClick={() => setPage("dashboard")}>Dashboard</button>
